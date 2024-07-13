da5bc063013f77c0d627e3b2fa696e523f795f80
Pink Flumph <pinkflumph@gmail.com>
2024-07-13 16:49:24 +0400

    Update court-of-blades-character-sheet.js
    Reverted height to fixed to allow resizing

--- scripts/court-of-blades-character-sheet.js
+++ scripts/court-of-blades-character-sheet.js
@@ -443,7 +443,7 @@ export class CourtOfBladesCharacterSheet extends CourtOfBladesSheet {
       classes: ["court-of-blades", "sheet", "pc", "actor"],
       template: "modules/court-of-blades/templates/character-sheet.hbs",
       width: 800,
-      height: "auto",
+      height: 1200,
       tabs: [{ navSelector: ".tabs", contentSelector: ".tab-content", initial: "playbook" }]
     });
   }
